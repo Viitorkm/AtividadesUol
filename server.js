c16386ac6a1021edf020e94c5f6eae2e0e94175c
Vitor Km <vhkm12@gmail.com>
2025-03-20 11:53:35 -0300

    Primeiro Endpoint - /Health-check

---
+++ server.js
@@ -0,0 +1,33 @@
+const { createServer, request } = require("node:http");
+const { URL } = require("node:url");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = createServer((request, response) => {
+  response.setHeader("Content-Type", "application/json");
+
+  try {
+    const url = new URL(request.url, `http://${hostname}:${port}`);
+
+    if (url.pathname === "/health-check") {
+      response.statusCode = 200;
+      const resposta = {
+        success: true,
+        timestamp: new Date().toISOString(),
+      };
+
+      response.end(JSON.stringify(resposta));
+    } else {
+      response.statusCode = 404;
+      response.end("Rota não encontrada");
+    }
+  } catch (error) {
+    response.statusCode = 500;
+    response.end(JSON.stringify({ error: "Erro interno do servidor" }));
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}/rota1`);
+});
