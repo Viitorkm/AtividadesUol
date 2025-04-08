48881fbbb755471fa8edaa3d3795e93751506efb
Vitor Km <vhkm12@gmail.com>
2025-04-07 21:46:54 -0300

    Adicionando Primeiros Arquivos

--- NumeroPrimo/main.js
+++ NumeroPrimo/main.js
@@ -1,21 +1,18 @@
-let numero = 1
-let divisores = 0
+let numero = 3;
+let divisores = 0;
 
-function verificarPrimo(numero){
-    if(!Number.isInteger(numero)){
-        return console.log("valor invalido detectado")
-    }
-    
-    for(let i=1 ; i<=numero ; i++)
-        if(numero % i == 0)
-            divisores++;
-    
-    if(divisores == 2){
-        return true
-    }else{
-        return false
-    }
-    
+function verificarPrimo(numero) {
+  if (!Number.isInteger(numero)) {
+    return console.log("valor invalido detectado");
+  }
+
+  for (let i = 1; i <= numero; i++) if (numero % i == 0) divisores++;
+
+  if (divisores == 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(verificarPrimo(numero))
+console.log(verificarPrimo(numero));
